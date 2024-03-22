96fdb48b40fac26b495de7704574f7c461be83fb
Shruti Mallick <53283341+shrutimallick@users.noreply.github.com>
2024-03-22 20:34:52 +0530

    Add files via upload

---
+++ Highradius/dashboard1/dataset.js
@@ -0,0 +1,952 @@
+var dataset = [{
+	"monthDate": "2018-05",
+	"count": 83,
+	"carrier": "UPS akash",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 23,
+	"carrier": "Averitt rahul",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 1,
+	"carrier": "Averittashok",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 14424,
+	"carrier": "Loomwuwi",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 214,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 63,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 112372,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 52,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-06",
+	"count": 9,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-07",
+	"count": 11,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-07",
+	"count": 6154,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-07",
+	"count": 200,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-07",
+	"count": 57469,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-07",
+	"count": 216,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-07",
+	"count": 5,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-08",
+	"count": 18,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-08",
+	"count": 11143,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-08",
+	"count": 54,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-08",
+	"count": 100351,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-08",
+	"count": 49,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-08",
+	"count": 16,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 49,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 1,
+	"carrier": "Averitt Express",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 17,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 13469,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 272,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 216,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 96,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 51,
+	"carrier": "UPS Freight",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 621,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 85700,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 23,
+	"carrier": "UPS USA",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 85,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-09",
+	"count": 14,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 44,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 1,
+	"carrier": "Averitt Express",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 24993,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 470,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 20,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 40,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 6,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 20119,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 17,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 15,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-10",
+	"count": 1,
+	"carrier": "YRC",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 28,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 4,
+	"carrier": "Averitt Express",
+	"error_sub_type": "To be reprocessed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 5847,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 149,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 21,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 19,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 5,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 224674,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 165,
+	"carrier": "UPS USA",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 96,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-11",
+	"count": 27,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 12,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 1,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 16951,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 489,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 4,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 1,
+	"carrier": "UPS Freight",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 4,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 159649,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 110,
+	"carrier": "UPS USA",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 2257,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 22,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2018-12",
+	"count": 10,
+	"carrier": "YRC",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 9,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 12,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 5189,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 2,
+	"carrier": "UPS Freight",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 67903,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 519,
+	"carrier": "UPS USA",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 2412,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 7,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-01",
+	"count": 1,
+	"carrier": "YRC",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 13,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 1,
+	"carrier": "UPS Freight",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 110013,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 64,
+	"carrier": "UPS USA",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 2169,
+	"carrier": "UPS USA",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 1464,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-02",
+	"count": 8,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 17,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 1,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 23018,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 287,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 19,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 87003,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 67,
+	"carrier": "UPS USA",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 14,
+	"carrier": "UPS USA",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 95,
+	"carrier": "UPS USA",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-03",
+	"count": 24,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 11,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 2,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 7522,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 2552,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 1246,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 2,
+	"carrier": "Loomis",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 74260,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 63,
+	"carrier": "UPS USA",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-04",
+	"count": 14,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 8,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 2,
+	"carrier": "Averitt Express",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 5,
+	"carrier": "Averitt Express",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 10148,
+	"carrier": "Loomis",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 528,
+	"carrier": "Loomis",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 8881,
+	"carrier": "Loomis",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 83297,
+	"carrier": "UPS USA",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 340,
+	"carrier": "UPS USA",
+	"error_sub_type": "Aggregation started. Not completed.",
+	"error_type": "Aggregation started. Not completed.",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 179,
+	"carrier": "UPS USA",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 200,
+	"carrier": "UPS USA",
+	"error_sub_type": "To be reprocessed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 12980,
+	"carrier": "UPS USA",
+	"error_sub_type": "Invalid Tracking Number",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 3969,
+	"carrier": "UPS USA",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 4994,
+	"carrier": "UPS USA",
+	"error_sub_type": "In Transit",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 4,
+	"carrier": "YRC",
+	"error_sub_type": "Success",
+	"error_type": "Success",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 3,
+	"carrier": "YRC",
+	"error_sub_type": "POD does not exist",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	},
+	{
+	"monthDate": "2019-05",
+	"count": 15,
+	"carrier": "YRC",
+	"error_sub_type": "Failed",
+	"error_type": "Failed",
+	"aggregation_type": "Web"
+	}];
